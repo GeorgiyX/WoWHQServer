44e9487503d364544e9e899c74a1ab1721ed8b66
y <y@ru.net>
2019-05-02 20:47:12 +0400

    Test2

--- server.py
+++ server.py
@@ -1,8 +1,4 @@
 from Core import app
-import sys
 
 if __name__=="__main__":
-    if sys.argv[1] == 'l':
-        app.run(debug=app.config.get('DEBUG'), port = 8000)
-    else:
-        app.run(debug=app.config.get('DEBUG'))
+    app.run(debug=app.config.get('DEBUG'))
